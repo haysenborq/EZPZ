1c0ec3d5d181ed89e631590a1fb0d6e010a83481
haysenborq <silberbach@outlook.com>
2015-07-29 03:16:12 +0200

    Just a test

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <title>EZPZ</title>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+</head>
+<body>
+<nav>
+<div id="nav">
+
+</div>
+</nav>
+</body>
+</html>
